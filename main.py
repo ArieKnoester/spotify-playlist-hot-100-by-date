a24d83fe3641a12e1c6603ba827ee4e58000acb4
Arie Knoester <arikstr@gmail.com>
2023-12-20 14:23:35 -0600

    Added Spotify environment variables.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # Spotify developer's dashboard (must have an account):
 # https://developer.spotify.com/dashboard/
+# Spotipy module documentation:
+# https://spotipy.readthedocs.io/en/2.22.1/
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+from spotipy import oauth2
 
+load_dotenv(".env")
 BILLBOARD_HOT_100_URL = "https://www.billboard.com/charts/hot-100/"
-
+SPOTIFY_CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
+SPOTIFY_CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
+SPOTIFY_REDIRECT_URL = os.environ["SPOTIFY_REDIRECT_URL"]
 
 user_date = input("Which year do you want to travel to? Type the date in YYYY-MM-DD format:\n")
 response = requests.get(url=f"{BILLBOARD_HOT_100_URL}{user_date}")
