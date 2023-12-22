34b6282e3a38341eccf5547460fb99cceab95a1b
Arie Knoester <arikstr@gmail.com>
2023-12-22 14:12:11 -0600

    Added artist name to scraping. Added search calls to Spotify's api.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 from spotipy.oauth2 import SpotifyOAuth
+import time  # debugging. Slow down the api calls to Spotify to make it easier to track in the console.
+import pprint as pp  # debugging.
 
 load_dotenv(".env")
 BILLBOARD_HOT_100_URL = "https://www.billboard.com/charts/hot-100/"
@@ -22,6 +24,8 @@ response.raise_for_status()
 billboard_content = response.text
 soup = BeautifulSoup(billboard_content, "html.parser")
 song_titles = [title.get_text().strip() for title in soup.select("li h3", limit=100)]
+artists = [title.next_element.next_element.next_element.get_text().strip() for title in soup.select("li h3", limit=100)]
+spotify_search_terms = list(zip(song_titles, artists))
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
@@ -31,5 +35,40 @@ sp = spotipy.Spotify(
         scope=SPOTIFY_OAUTH_SCOPE
     )
 )
-user_id = sp.current_user()["id"]
-print(user_id)
+# user_id = sp.current_user()["id"]
+# # print(user_id)
+
+year = user_date.split("-")[0]
+spotify_song_uris = []
+
+for search_term in spotify_search_terms:
+    # print(search_term[0], search_term[1])
+    search_result = sp.search(
+        q=f"{search_term[0]} {search_term[1]} {year}",
+        type="track",
+        limit=1
+    )
+
+    try:
+        song_uri = search_result["tracks"]["items"][0]["uri"]
+    except IndexError:
+        print(f"Something went wrong.")
+        print(f"search_term: {search_term}")
+        print(search_result)
+    else:
+        spotify_song_uris.append(song_uri)
+    # debugging.
+    # time.sleep(1)
+
+# pp.pprint(spotify_song_uris)
+# print(len(spotify_song_uris))
+
+# Debugging.
+# search_term = ""
+# uri = sp.search(
+#     q=f"{search_term} {year}",
+#     type="track",
+#     limit=1
+# )
+# pp.pprint(uri)
+# print(uri["tracks"]["items"][0]["uri"])
